3a2dd9d6183e1726014cde6f256825294434cbc2
Jonathan001-cmd <77310361+Jonathan001-cmd@users.noreply.github.com>
2021-02-08 18:42:32 -0400

    Entidades. Close  #1.

---
+++ Parcial1-ap1-20180240/Entidades/Registro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Parcial1_ap1_20180240.Entidades
+{
+    class Registro
+    {
+        [Key]
+        public string Ciudad { get; set; }
+
+    }
+}
